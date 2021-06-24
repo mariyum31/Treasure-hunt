d780ea9801cf9da3e539cd89e2591a9bc2d6281b
mariyum31 <77669337+mariyum31@users.noreply.github.com>
2021-06-24 15:21:25 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,23 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    text("Hint:ATADASBA!!",100,370)
+    text("Hint:CUTNIFON!!",700,270)
+
+// add code for displaying rest of the names and the hints.
+ //   if(actualCode === enteredCode.toUpperCase()){
+   //     return true
+       // else{
+         //   return false
+        //}
+  //  }
+
+}
